2cb4534afc499585396e039c0ba62d2b32ab4800
fnaz97 <57403174+fnaz97@users.noreply.github.com>
2019-12-03 01:13:05 +0800

    Update details.php

--- details.php
+++ details.php
@@ -1,16 +1,15 @@
+
 <?php
 include 'includes/connect.php';
 $user_id = $_SESSION['user_id'];
 
-$result = mysqli_query($con, "SELECT * FROM wallet_details LEFT JOIN users ON users.id=wallet_details.id  where users.id = $user_id");
+$result = mysqli_query($con, "SELECT * FROM users where id = $user_id");
 while($row = mysqli_fetch_array($result)){
 $name = $row['name'];	
 $address = $row['address'];
 $contact = $row['contact'];
 $email = $row['email'];
 $username = $row['username'];
-$cc_number = $row['number'];
-$cvv = $row['cvv'];
 }
 	if($_SESSION['customer_sid']==session_id())
 	{
@@ -131,7 +130,7 @@ $cvv = $row['cvv'];
                 </div>
 				 <div class="col col s8 m8 l8">
                     <ul id="profile-dropdown" class="dropdown-content">
-                        <li><a href="routers/logout.php"><i class="mdi-hardware-keyboard-tab"></i> Logout</a>
+                        <li><a href="routers/logout.php">Logout</a>
                         </li>
                     </ul>
                 </div>
@@ -141,11 +140,11 @@ $cvv = $row['cvv'];
                 </div>
             </div>
             </li>
-            <li class="bold"><a href="home.php" class="waves-effect waves-cyan"><i class="mdi-editor-border-color"></i> Order Food</a>
+            <li class="bold"><a href="home.php" class="waves-effect waves-cyan">Order Food</a>
             </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-editor-insert-invitation"></i> Orders</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan">Orders</a>
                             <div class="collapsible-body">
                                 <ul>
 								<li><a href="orders.php">All Orders</a>
@@ -164,7 +163,7 @@ $cvv = $row['cvv'];
                 </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-action-question-answer"></i> Tickets</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"> Tickets</a>
                             <div class="collapsible-body">
                                 <ul>
 								<li><a href="tickets.php">All Tickets</a>
@@ -181,7 +180,7 @@ $cvv = $row['cvv'];
                         </li>
                     </ul>
                 </li>			
-            <li class="bold active"><a href="details.php" class="waves-effect waves-cyan"><i class="mdi-social-person"></i> Edit Details</a>
+            <li class="bold active"><a href="details.php" class="waves-effect waves-cyan"> Edit Details</a>
             </li>			
         </ul>
         <a href="#" data-activates="slide-out" class="sidebar-collapse btn-floating btn-medium waves-effect waves-light hide-on-large-only cyan"><i class="mdi-navigation-menu"></i></a>
@@ -220,7 +219,7 @@ $cvv = $row['cvv'];
                     <form class="formValidate" id="formValidate" method="post" action="routers/details-router.php" novalidate="novalidate"class="col s12">
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-action-account-circle prefix"></i>
+                          
                           <input name="username" id="username" type="text" value="<?php echo $username;?>" data-error=".errorTxt1">
                           <label for="username" class="">Username</label>
 						  <div class="errorTxt1"></div>
@@ -228,7 +227,7 @@ $cvv = $row['cvv'];
                       </div>					
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-action-account-circle prefix"></i>
+                          
                           <input name="name" id="name" type="text" value="<?php echo $name;?>" data-error=".errorTxt2">
                           <label for="name" class="">Name</label>
 						   <div class="errorTxt2"></div>
@@ -236,7 +235,7 @@ $cvv = $row['cvv'];
                       </div>
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-communication-email prefix"></i>
+                          
                           <input name="email" id="email" type="email" value="<?php echo $email;?>" data-error=".errorTxt3">
                           <label for="email" class="">Email</label>
 						  <div class="errorTxt3"></div>
@@ -244,7 +243,7 @@ $cvv = $row['cvv'];
                       </div>
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-action-lock-outline prefix"></i>
+                       
                           <input name="password" id="password" type="password" data-error=".errorTxt4">
                           <label for="password" class="">Password</label>
 						  <div class="errorTxt4"></div>
@@ -252,37 +251,15 @@ $cvv = $row['cvv'];
                       </div>
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-action-account-circle prefix"></i>
+                         
                           <input name="phone" id="phone" type="number" value="<?php echo $contact;?>" data-error=".errorTxt5">
                           <label for="phone" class="">Contact</label>
 						  <div class="errorTxt5"></div>
                         </div>
-                      </div>		
-
-                          <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-action-account-circle prefix"></i>
-                          <input name="cc_number" id="cc_number" type="number" value="<?php echo $cc_number;?>" data-error=".errorTxt5" readonly>
-                          <label for="phone" class="">Credit Card Number</label>
-              <div class="errorTxt5"></div>
-                        </div>
-                      </div>  
-
-
-                          <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-action-account-circle prefix"></i>
-                          <input name="cvv" id="cvv" type="number" value="<?php echo $cvv;?>" data-error=".errorTxt5" readonly>
-                          <label for="phone" class="">CVV Number</label>
-              <div class="errorTxt5"></div>
-                        </div>
-                      </div>  
-
-
-
+                      </div>					  
                       <div class="row">
                         <div class="input-field col s12">
-                          <i class="mdi-action-home prefix"></i>
+                         
                           <textarea name="address" id="address" class="materialize-textarea validate" data-error=".errorTxt6"><?php echo $address;?></textarea>
                           <label for="address" class="">Address</label>
 						  <div class="errorTxt6"></div>
@@ -290,7 +267,7 @@ $cvv = $row['cvv'];
                         <div class="row">
                           <div class="input-field col s12">
                             <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
-                              <i class="mdi-content-send right"></i>
+                              
                             </button>
                           </div>
                         </div>
