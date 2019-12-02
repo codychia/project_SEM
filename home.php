1d536ace879070a1c940c045540d44fac3b9c889
fnaz97 <57403174+fnaz97@users.noreply.github.com>
2019-12-03 00:24:00 +0800

    Update home.php

--- home.php
+++ home.php
@@ -136,11 +136,11 @@ include 'includes/wallet.php';
                 </div>
             </div>
             </li>
-            <li class="bold active"><a href="home.php" class="waves-effect waves-cyan"><i class="mdi-editor-border-color"></i> Order Food</a>
+            <li class="bold active"><a href="home.php" class="waves-effect waves-cyan"> Order Food</a>
             </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-editor-insert-invitation"></i> Orders</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan">Orders</a>
                             <div class="collapsible-body">
                                 <ul>
 								<li><a href="orders.php">All Orders</a>
@@ -159,7 +159,7 @@ include 'includes/wallet.php';
                 </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-action-question-answer"></i> Tickets</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"> Tickets</a>
                             <div class="collapsible-body">
                                 <ul>
 								<li><a href="tickets.php">All Tickets</a>
@@ -176,7 +176,7 @@ include 'includes/wallet.php';
                         </li>
                     </ul>
                 </li>					
-            <li class="bold"><a href="details.php" class="waves-effect waves-cyan"><i class="mdi-social-person"></i> Edit Details</a>
+            <li class="bold"><a href="details.php" class="waves-effect waves-cyan"> Edit Details</a>
             </li>				
         </ul>
         <a href="#" data-activates="slide-out" class="sidebar-collapse btn-floating btn-medium waves-effect waves-light hide-on-large-only cyan"><i class="mdi-navigation-menu"></i></a>
@@ -236,7 +236,7 @@ include 'includes/wallet.php';
 </table>
               </div>
 			  <div class="input-field col s12">
-              <i class="mdi-editor-mode-edit prefix"></i>
+              
               <textarea id="description" name="description" class="materialize-textarea"></textarea>
               <label for="description" class="">Any note(optional)</label>
 			  </div>
